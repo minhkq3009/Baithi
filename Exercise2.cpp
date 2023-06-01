a33eea2dcf8e2d3cef3d524b2affc847ab5c6ec3
minhkq3009 <55178535+minhkq3009@users.noreply.github.com>
2023-06-01 09:03:31 +0700

    Add files via upload

---
+++ Exercise2.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int isEven(int number) {
+    if (number % 2 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    int n, m, number;
+    
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (isEven(number)) {
+        printf("%d is an even number.\n", number);
+    } else {
+        printf("%d is an odd number.\n", number);
+    }
+
+    printf("Please, enter a number (n and m): ");
+    scanf("%d %d", &n, &m);
+
+    printf("Even numbers between %d and %d:\n", n, m);
+    for (int i = n; i <= m; i++) {
+        if (isEven(i)) {
+            printf("%d\n", i);
+        }
+    }
+	    
+    return 0;
+}
+
